20bedc4efba06e20306ac81d3584bfccd521e53b
JGW-Korea <jgw6372@gmail.com>
2022-08-28 00:18:21 +0900

    소개 페이지(완료) & 스크롤 이벤트(X)

--- client/src/pages/LandingPage/Sections/IntroductionSection/index.js
+++ client/src/pages/LandingPage/Sections/IntroductionSection/index.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-import { Container } from "./styled";
+import {
+  Container,
+  InfoContainer,
+  InfoContainer_Text,
+  InfoContainer_Image,
+} from "./styled";
 
 function IntroductionSection() {
-  return <Container>IntroductionSection</Container>;
+  return (
+    <Container>
+      <img
+        className="Background_Img"
+        alt="Background_Img"
+        src={require("../../../../assets/img/Background/Background.png")}
+      />
+      <InfoContainer>
+        <InfoContainer_Text>
+          <h2 className="InfoContainer_Text_Header">
+            목소리로 나에게 어울리는 노래 장르와 하나의 곡을 완성시켜보자
+          </h2>
+          <span className="InfoContainer_Text_Span">맞춤형 작사작곡</span>
+          <button className="InfoContainer_Text_Btn">시작하기</button>
+        </InfoContainer_Text>
+        <InfoContainer_Image>
+          <img
+            className="Background_Icon"
+            alt="Background_Icon"
+            src={require("../../../../assets/img/Background/BacgroundIcon.png")}
+          />
+        </InfoContainer_Image>
+      </InfoContainer>
+    </Container>
+  );
 }
 
 export default IntroductionSection;
--- client/src/pages/LandingPage/Sections/IntroductionSection/styled.js
+++ client/src/pages/LandingPage/Sections/IntroductionSection/styled.js
@@ -1,12 +1,72 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  /* margin-top: 64px; */
+  position: relative;
+  width: 100%;
+  height: 100vh;
+
+  .Background_Img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const InfoContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  /* justify-content: space-between; */
+  top: 0px;
+`;
+
+export const InfoContainer_Text = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  max-width: 100%;
+  top: 45%;
+
+  margin-left: 100px;
+  word-break: keep-all;
+
+  .InfoContainer_Text_Header {
+    font-size: 35px;
+    margin-bottom: 40px;
+    color: #747474;
+  }
+
+  .InfoContainer_Text_Span {
+    display: block;
+    font-size: 30px;
+    margin-bottom: 40px;
+    color: #767676;
+  }
+
+  .InfoContainer_Text_Btn {
+    border: none;
+    width: 240px;
+    padding: 20px;
+    font-size: 20px;
+    border-radius: 20px;
+    background-color: #fcfaf5;
+    box-shadow: 0px 0px 3px rgba(203, 203, 203, 0.5),
+      inset 1px 0px 2px rgba(239, 239, 239, 0.9);
+    cursor: pointer;
+  }
+`;
+
+export const InfoContainer_Image = styled.div`
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  margin-top: 64px;
-  height: 96vh;
-  background-color: white;
-  color: black;
+
+  .Background_Icon {
+    max-width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
